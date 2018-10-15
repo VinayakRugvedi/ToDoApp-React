11032aa8fab1577542b70d6da58eae475a116f53
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-10-15 12:13:15 +0530

    Add Script to play text on load

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+document.body.onload = playFunc
+function playFunc () {
+  responsiveVoice.speak("Hello Procrastinators, welcome to the secret diary of your dreams!", "UK English Male")
+}
+
 // To handle the modal window
 var closer = document.querySelector('.close')
 closer.addEventListener('click', () => {
